59513749e882cb7efc74d5929e334ca353ece5ae
DaniilShaposhnikow <87771174+DaniilShaposhnikow@users.noreply.github.com>
2021-08-14 14:55:52 +0200

    Android-Study-Toolbar
    Android-Study-Toolbar

---
+++ app/src/main/java/com/hfad/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.hfad.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+//import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
